74341b9cf39d0a0496a4940ecfe370e07d61b06f
sydist <81040138+sydist@users.noreply.github.com>
2023-06-24 12:28:17 +0200

    feat: mockup a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Navigation
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
